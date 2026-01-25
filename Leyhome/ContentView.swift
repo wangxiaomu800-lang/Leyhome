2bba8f8dd3c54e66a460bfb2c668977233636992
Sherry Wang <wangxiaomu800@gmail.com>
2026-01-25 19:28:45 +0800

    Initial Commit

---
+++ Leyhome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Leyhome
+//
+//  Created by 王璇 on 2026/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Leyhome/LeyhomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeyhomeApp.swift
+//  Leyhome
+//
+//  Created by 王璇 on 2026/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LeyhomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
